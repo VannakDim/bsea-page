27200c74e5f2b247c9a87731e40c447f2aaf6482
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-02-05 02:19:05 +0700

    added language switcher

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
+app.get("/home",function(req,res){
   res.render("index");
 });
 
